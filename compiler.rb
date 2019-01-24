6bc020ba52d18686f0aa8b26bf789b4c02aea67d
Matt <matt_burgoyne@live.co.uk>
2019-01-24 17:40:02 +0000

    begin adding to token type arrays

--- compiler.rb
+++ compiler.rb
@@ -1,4 +1,18 @@
 ## lexer (tokenizer) -> parser -> code generator
 
 class Tokenizer
+  TOKEN_TYPES = [/\bdef\b/]
+  def initialize(code)
+      @code = code
+  end
+
+  def tokenize
+    until @code.empty?
+      TOKEN_TYPES.each do |type|
+        end
+    end
+  end
 end
+
+tokens = Tokenizer.new(File.read("test.src")).tokenize
+p tokens
