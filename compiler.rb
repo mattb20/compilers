d33ba226fc3e66aaa50c9832d85647da07d76087
Matt <matt_burgoyne@live.co.uk>
2019-01-24 17:56:21 +0000

    raise runtime error if no match found on tokenizer

--- compiler.rb
+++ compiler.rb
@@ -1,7 +1,7 @@
 ## lexer (tokenizer) -> parser -> code generator
 
 class Tokenizer
-  TOKEN_TYPES = [[:def, /\bdef\b/], [:end, /\bend\b/], /\b[a-zA-Z]+\b/]
+  TOKEN_TYPES = [[:def, /\bdef\b/], [:end, /\bend\b/], [:identifier, /\b[a-zA-Z]+\b/], [:integer, /\b[0-9]+\b/]]
   def initialize(code)
       @code = code
   end
@@ -11,6 +11,8 @@ class Tokenizer
       TOKEN_TYPES.each do |type, re|
         end
     end
+
+    raise RuntimeError.new("Couldn't match token on #{{@code.inspect}}")
   end
 end
 
