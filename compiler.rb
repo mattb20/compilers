99726fc1ca1d277d68ffda86d9f4880f74fae626
Matt <matt_burgoyne@live.co.uk>
2019-01-24 17:48:35 +0000

    add token types to regexes

--- compiler.rb
+++ compiler.rb
@@ -1,14 +1,14 @@
 ## lexer (tokenizer) -> parser -> code generator
 
 class Tokenizer
-  TOKEN_TYPES = [/\bdef\b/]
+  TOKEN_TYPES = [[:def, /\bdef\b/], [:end, /\bend\b/], /\b[a-zA-Z]+\b/]
   def initialize(code)
       @code = code
   end
 
   def tokenize
     until @code.empty?
-      TOKEN_TYPES.each do |type|
+      TOKEN_TYPES.each do |type, re|
         end
     end
   end
