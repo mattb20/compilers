541d4d418030f3f2d6a438bf4488b75a29c027d9
mattb20 <matt_burgoyne@live.co.uk>
2019-02-13 18:43:34 +0000

    return new token after match

--- compiler.rb
+++ compiler.rb
@@ -10,6 +10,11 @@ class Tokenizer
   def tokenize
     until @code.empty?
       TOKEN_TYPES.each do |type, match|
+          match = /\A(#{match})/
+          if code =~ match
+            value = $1
+            @code = @code[value.length..-1]
+            Token.new(type, value)
         end
     end
     raise RuntimeError.new("Couldn't match token on #{{@code.inspect}}")
