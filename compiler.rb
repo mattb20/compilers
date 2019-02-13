90e96d7bb8318a1b666ca7519cbd9228a92f0c65
mattb20 <matt_burgoyne@live.co.uk>
2019-02-13 18:39:53 +0000

    add paren to token types

--- compiler.rb
+++ compiler.rb
@@ -1,14 +1,15 @@
 ## lexer (tokenizer) -> parser -> code generator
 
 class Tokenizer
-  TOKEN_TYPES = [[:def, /\bdef\b/], [:end, /\bend\b/], [:identifier, /\b[a-zA-Z]+\b/], [:integer, /\b[0-9]+\b/]]
+  TOKEN_TYPES = [[:def, /\bdef\b/], [:end, /\bend\b/], [:identifier, /\b[a-zA-Z]+\b/], [:integer, /\b[0-9]+\b/],
+[:oparen, /\(/], [:cparen, /\)/]]
   def initialize(code)
       @code = code
   end
 
   def tokenize
     until @code.empty?
-      TOKEN_TYPES.each do |type, re|
+      TOKEN_TYPES.each do |type, match|
         end
     end
     raise RuntimeError.new("Couldn't match token on #{{@code.inspect}}")
