5b3538b59ea429ab6f9f1eedbf733e5904f53ce5
Matthew Burgoyne <matt_burgoyne@live.co.uk>
2021-01-25 16:29:15 +0000

    have all token types defined

--- compiler.rb
+++ compiler.rb
@@ -1,23 +1,17 @@
-## lexer (tokenizer) -> parser -> code generator
-
 class Tokenizer
-  TOKEN_TYPES = [[:def, /\bdef\b/], [:end, /\bend\b/], [:identifier, /\b[a-zA-Z]+\b/], [:integer, /\b[0-9]+\b/],
-[:oparen, /\(/], [:cparen, /\)/]]
+  TOKEN_TYPES = [[:def, /\bdef\b/] ,
+    [:end, /\bend\b/],
+    [:identifier, /\b[a-zA-Z]+\b/], 
+    [:integer, /\b[0-9]+\b/], 
+    [:openparen, /\(/], 
+    [:closeparen, /\)/]
+  ]
   def initialize(code)
       @code = code
   end
 
   def tokenize
-    until @code.empty?
-      TOKEN_TYPES.each do |type, match|
-          match = /\A(#{match})/
-          if code =~ match
-            value = $1
-            @code = @code[value.length..-1]
-            Token.new(type, value)
-        end
-    end
-    raise RuntimeError.new("Couldn't match token on #{{@code.inspect}}")
+    TOKEN_TYPES
   end
 end
 
