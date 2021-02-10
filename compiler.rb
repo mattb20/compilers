a328b18b70ec4368e1a39832fffa311adeef7a00
Matthew Burgoyne <matt_burgoyne@live.co.uk>
2021-02-10 08:44:07 +0000

    complete tokenizer

--- compiler.rb
+++ compiler.rb
@@ -1,5 +1,6 @@
 class Tokenizer
-  TOKEN_TYPES = [[:def, /\bdef\b/] ,
+  TOKEN_TYPES = [
+    [:def, /\bdef\b/],
     [:end, /\bend\b/],
     [:identifier, /\b[a-zA-Z]+\b/], 
     [:integer, /\b[0-9]+\b/], 
@@ -11,9 +12,27 @@ class Tokenizer
   end
 
   def tokenize
-    TOKEN_TYPES
+    tokens = []
+    until @code.empty?
+      tokens << get_first_token
+      @code = @code.strip
+    end
+    tokens
+  end
+  def get_first_token
+    TOKEN_TYPES.each do |type, regex|
+      re = /\A(#{regex})/
+      if @code  =~ re
+        value = $1
+        @code = @code[value.length..-1]
+        return Token.new(type, value)
+      end
+    end
+    raise RuntimeError.new("Couldn't match token on #{@code.inspect}")
   end
 end
 
+Token = Struct.new(:type, :value)
+
 tokens = Tokenizer.new(File.read("test.src")).tokenize
-p tokens
+puts tokens.map(&:inspect).join("\n")
